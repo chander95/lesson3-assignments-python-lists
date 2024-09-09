c25c594d3436dd345eda5ee5948af421b702647b
Alex Salmeron <juanasalmeron95@gmail.com>
2024-09-09 14:51:29 -0400

    task 1 completed

---
+++ advanced-slicing-techniques.py
@@ -0,0 +1,5 @@
+# Task 1: Given the list of temperatures, Extract the temperatures for the second week (7 days) of the month (index 7 to index 14). 
+# Expected Outcome: 83, 85, 86, 87, 88, 89, 90,
+
+temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+print(temperatures[7:14])
